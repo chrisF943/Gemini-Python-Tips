8213035d6a2c9b4fd5de6b8290fe6c53645d10f9
Christopher Faris <142948338+chrisF943@users.noreply.github.com>
2025-01-31 14:21:02 -0500

    consolidated script into functions, added logging, added codeowners file

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from email.mime.text import MIMEText
 import os
 import google.generativeai as genai
 import datetime
+import logging
+
+# Configure logging
+logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s %(message)s")
 
 today = datetime.date.today()
 day = today.weekday()
@@ -12,45 +16,60 @@ tls_providers = ["Outlook", "Yahoo"]
 
 chosen_provider = "gmail"
 
-if day == 0 or day == 2 or day == 4:
-    genai.configure(api_key=os.getenv("API_KEY"))
 
-    model = genai.GenerativeModel("gemini-1.5-flash")
-    response = model.generate_content(
-        """
-        Give me 3 small, intermediate to advanced Python tips and tricks to make
-        my projects better and more efficient. Please give me exactly 3 unique, different
-        tips everytime you are asked this prompt.
+def send_prompt():
+    if day == 0 or day == 2 or day == 4:
+        logging.info("Sending prompt...")
+        genai.configure(api_key=os.getenv("API_KEY"))
 
-        """
-    )
+        model = genai.GenerativeModel("gemini-1.5-flash")
+        prompt = model.generate_content(
+            """
+            Give me 3 small, intermediate to advanced Python tips and tricks to make
+            my projects better and more efficient. Please give me exactly 3 unique, different
+            tips everytime you are asked this prompt.
+    
+            """
+        )
+        generated_response = prompt.text
+        logging.info("send_prompt() executed successfully.")
+        return generated_response
+    else:
+        logging.info("Skipped execution today.")
+        return None
 
-    print(response.text)
 
-    subject = "Your Daily Python Tips"
-    msg = MIMEText(response.text, "plain", "utf-8")
+def send_email(response):
+    logging.info("Sending email...")
+    msg = MIMEText(response, "plain", "utf-8")
     msg["Subject"] = "Your Daily Python Tips"
     msg["From"] = os.getenv("SENDER")
     msg["To"] = os.getenv("RECIPIENT")
 
-    if chosen_provider.lower() in [provider.lower() for provider in ssl_providers]:
-        print("you chose ssl")
-        with smtplib.SMTP_SSL("smtp.gmail.com", 465) as connection:
-            connection.login(os.getenv("SENDER"), os.getenv("PASSWORD"))
-            connection.sendmail(os.getenv("SENDER"), os.getenv("RECIPIENT"), msg.as_string())
+    try:
+        if chosen_provider.lower() in [provider.lower() for provider in ssl_providers]:
+            logging.info("SSl was chosen.")
+            with smtplib.SMTP_SSL("smtp.gmail.com", 465) as connection:
+                connection.login(os.getenv("SENDER"), os.getenv("PASSWORD"))
+                connection.sendmail(os.getenv("SENDER"), os.getenv("RECIPIENT"), msg.as_string())
 
-    elif chosen_provider.lower() in [provider.lower() for provider in tls_providers]:
-        print("you chose tls")
-        with smtplib.SMTP("smtp.gmail.com", 587) as connection:
-            connection.starttls()
-            connection.login(os.getenv("SENDER"), os.getenv("PASSWORD"))
-            connection.sendmail(os.getenv("SENDER"), os.getenv("RECIPIENT"), msg.as_string())
+        elif chosen_provider.lower() in [provider.lower() for provider in tls_providers]:
+            logging.info("TLS was chosen.")
+            with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+                connection.starttls()
+                connection.login(os.getenv("SENDER"), os.getenv("PASSWORD"))
+                connection.sendmail(os.getenv("SENDER"), os.getenv("RECIPIENT"), msg.as_string())
 
-    else:
-        print("Provider not recognized, please enter a provider from one of the lists.")
+        else:
+            logging.warning("Chosen provider not recognized, please choose a provider from one of the lists.")
 
-else:
-    print("No tips today.")
+        logging.info("Email sent successfully.")
+
+    except Exception as e:
+        logging.error(f"An error occurred: {e}")
+
 
 if __name__ == "__main__":
-    pass
+    prompt_response = send_prompt()
+    if prompt_response is not None:
+        send_email(prompt_response)
